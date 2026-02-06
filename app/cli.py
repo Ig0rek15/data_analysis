aa30eac31bc98f07982f61caada9b7181a1d847a
Igor <abaev-2022@mail.ru>
2026-02-06 19:39:38 +0300

    Added command(--files, --report).

---
+++ app/cli.py
@@ -0,0 +1,27 @@
+import argparse
+from typing import List, Tuple
+
+
+def parse_args() -> Tuple[List[str], str]:
+    '''
+    Разбирает аргументы командной строки.
+    '''
+    parser = argparse.ArgumentParser(
+        description='Скрипт для анализа макроэкономических данных'
+    )
+
+    parser.add_argument(
+        '--files',
+        nargs='+',
+        required=True,
+        help='Пути к CSV-файлам с экономическими данными'
+    )
+
+    parser.add_argument(
+        '--report',
+        required=True,
+        help='Название отчёта'
+    )
+
+    args = parser.parse_args()
+    return args.files, args.report
---
+++ main.py
@@ -0,0 +1,9 @@
+from app.cli import parse_args
+
+
+def main() -> None:
+    files, report_name = parse_args()
+
+
+if __name__ == '__main__':
+    main()
