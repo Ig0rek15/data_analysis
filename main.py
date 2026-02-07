1fbd5ff163844f313d3229a93d0663f05d2ddb50
Igor <abaev-2022@mail.ru>
2026-02-07 20:11:29 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import logging
+
+from tabulate import tabulate
+
 from app.cli import parse_args
 from app.loader import load_data
 from app.reports import REPORTS
 
-from tabulate import tabulate
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] %(message)s'
+)
 
 
 def main() -> None:
-    files, report_name = parse_args()
+    try:
+        files, report_name = parse_args()
+        logging.info('Запуск отчёта: %s', report_name)
 
-    if report_name not in REPORTS:
-        raise ValueError(f'Неизвестный отчёт: {report_name}')
+        if report_name not in REPORTS:
+            raise ValueError(f'Неизвестный отчёт: {report_name}')
 
-    data = load_data(files)
-    report = REPORTS[report_name]
+        data = load_data(files)
+        logging.info(f'Загружено строк данных: {len(data)}')
 
-    table = report.generate(data)
-    print(tabulate(table, headers=report.headers, tablefmt='grid'))
+        report = REPORTS[report_name]
+        table = report.generate(data)
+
+        print(tabulate(table, headers=report.headers, tablefmt='github'))
+
+    except FileNotFoundError as exc:
+        logging.error(f'Файл не найден: {exc}')
+    except ValueError as exc:
+        logging.error(f'{exc}')
+    except Exception as exc:
+        logging.error(f'Неожиданная ошибка: {exc}')
 
 
 if __name__ == '__main__':
