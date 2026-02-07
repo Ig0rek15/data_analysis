19bfaf50527e664d1c0f3a9dd10e8733fe72b676
Igor <abaev-2022@mail.ru>
2026-02-07 19:15:02 +0300

    Added a report registry.

--- app/reports/average_gdp.py
+++ app/reports/average_gdp.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from typing import List, Dict, Any
 
-from app.reports.base import BaseReport
+from .base import BaseReport
 
 
 class AverageGDPReport(BaseReport):
